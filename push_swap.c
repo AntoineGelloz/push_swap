21cef89e786e99ebd24949dd9ac2031b307b538d
Antoine GELLOZ <agelloz@e1r9p10.42.fr>
2019-07-23 12:15:54 +0200

    Advancements

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: agelloz <agelloz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 11:33:53 by agelloz           #+#    #+#             */
-/*   Updated: 2019/07/22 16:46:48 by agelloz          ###   ########.fr       */
+/*   Updated: 2019/07/23 12:15:29 by agelloz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,89 +62,185 @@ int		ft_check_n(char **av, int i)
 	return (1);
 }
 
-void	ft_delete_stack(t_stack *s)
+void	ft_delete_stacks(t_stack *a, t_stack *b)
 {
 	t_number	*curr;
 
-	curr = s->first;
-	while (curr)
+	if (a)
 	{
-		s->first = s->first->next;
-		free(curr);
-		curr = NULL;
-		curr = s->first;
+		curr = a->first;
+		while (curr)
+		{
+			a->first = a->first->next;
+			free(curr);
+			curr = NULL;
+			curr = a->first;
+		}
+		free(a);
+		a = NULL;
 	}
-	free(s);
-	s = NULL;
+	if (b)
+	{
+		curr = b->first;
+		while (curr)
+		{
+			b->first = b->first->next;
+			free(curr);
+			curr = NULL;
+			curr = b->first;
+		}
+		free(b);
+		b = NULL;
+	}
 }
 
-t_stack	*ft_fill_a(int ac, char **av)
+int		ft_fill_a(t_stack **a, int ac, char **av)
 {
-	t_stack		*a;
 	t_number	*curr;
 	t_number	*prev;
 	int			i;
 
-	if (!(a = malloc(sizeof(*a))))
-		return (NULL);
 	i = 0;
 	while (++i < ac)
 	{
 		if (ft_check_n(av, i))
 		{
 			if (!(curr = malloc(sizeof(*curr))))
-				return (NULL);
+				return (0);
 			(i != 1) ? prev->next = curr : 0;
 			curr->n = ft_atoi(av[i]);
 			curr->next = NULL;
-			(i == 1) ? a->first = curr : 0;
+			(i == 1) ? (**a).first = curr : 0;
 			prev = curr;
 			curr = curr->next;
 		}
 		else
 		{
 			ft_putstr("Error\n");
-			ft_delete_stack(a);
-			return (NULL);
+			return (0);
 		}
 	}
-	return (a);
+	printf("Filled.\n");
+	return (1);
 }
 
 t_stack	*ft_swap(t_stack **s)
 {
-		t_number	*one;
-		t_number	*two;
+	t_number	*one;
+	t_number	*two;
 
-		one = (**s).first;
-		if (one->next)
-		{
-			two = one->next;
-			one->next = (two->next) ? two->next : NULL;
-			two->next = one;
-			(**s).first = two;
-		}
-		return (*s);
+	one = (**s).first;
+	if (one->next)
+	{
+		two = one->next;
+		one->next = (two->next) ? two->next : NULL;
+		two->next = one;
+		(**s).first = two;
+	}
+	return (*s);
+}
+
+t_stack	*ft_rotate(t_stack **s)
+{
+	t_number	*one;
+	t_number	*curr;
+
+	one = (**s).first;
+	curr = (**s).first;
+	while (curr->next)
+		curr = curr->next;
+	((**s).first->next) ? (**s).first = (**s).first->next : 0;
+	curr->next = one;	
+	curr->next->next = NULL;
+	return (*s);
+}
+
+t_stack	*ft_reverse_rotate(t_stack **s)
+{
+	t_number	*one;
+	t_number	*curr;
+
+	one = (**s).first;
+	curr = (**s).first;
+	while (curr->next->next)
+		curr = curr->next;
+	(**s).first = curr->next;
+	curr->next->next = one;
+	curr->next = NULL;
+	return (*s);
+}
+
+t_stack	*ft_push(t_stack **s1, t_stack **s2)
+{
+	t_number	*one;
+	t_number	*pushed;
+
+	if ((**s2).first)
+	{
+		one = (**s1).first;
+		pushed = (**s2).first;
+		(**s1).first = pushed;
+		(**s2).first = pushed->next;
+		pushed->next = one;
+	}
+	return (*s1);
+}
+
+void	ft_display_stacks(t_stack *a, t_stack *b)
+{
+	t_number	*curr;
+
+	curr = a->first;
+	ft_putstr("Stack a: ");
+	while (curr)
+	{
+		ft_putnbr(curr->n);
+		ft_putchar(' ');
+		curr = curr->next;
+	}
+	ft_putchar('\n');
+
+	curr = (b->first) ? b->first : NULL;
+	ft_putstr("Stack b: ");
+	while (curr)
+	{
+		ft_putnbr(curr->n);
+		ft_putchar(' ');
+		curr = curr->next;
+	}
+	ft_putchar('\n');
+}
+
+void	ft_sort_1(t_stack **s1, t_stack **s2)
+{
+
 }
 
 int		main(int ac, char **av)
 {
 	t_stack		*a;
 	t_stack		*b;
-	t_number	*curr;
 
-	if (ac < 2 || !(a = ft_fill_a(ac, av)))
+	if (!(a = malloc(sizeof(*a))))
 		return (0);
-	a = ft_swap(&a);
-	curr = a->first;
-	b = NULL;
-	ft_putstr("Stack a:\n");
-	while (curr)
-	{
-		ft_putnbr(curr->n);
-		ft_putchar('\n');
-		curr = curr->next;
-	}
-	ft_delete_stack(a);
+	a->first = NULL;
+	if (!(b = malloc(sizeof(*b))))
+		return (0);
+	b->first = NULL;
+	if (ac < 2 || !ft_fill_a(&a, ac, av))
+		return (0);
+
+	//Tests
+	//a = ft_swap(&a);
+	//a = ft_rotate(&a);
+	//a = ft_reverse_rotate(&a);
+	//b = ft_push(&b, &a);
+	//b = ft_push(&b, &a);
+	//b = ft_push(&a, &b);
+	//b = ft_rotate(&b);
+
+	ft_sort_1(&a, &b);
+	ft_display_stacks(a, b);
+	ft_delete_stacks(a, b);
 	return (0);
 }
