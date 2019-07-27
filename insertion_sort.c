1126106ff89de2840e3d0f37b95a1ad0de7c25c8
Antoine GELLOZ <agelloz@e1r10p15.42.fr>
2019-07-27 12:05:28 +0200

    Advancements

--- insertion_sort.c
+++ insertion_sort.c
@@ -6,7 +6,7 @@
 /*   By: agelloz <agelloz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 11:33:53 by agelloz           #+#    #+#             */
-/*   Updated: 2019/07/26 21:04:42 by agelloz          ###   ########.fr       */
+/*   Updated: 2019/07/27 12:05:06 by agelloz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ void	ft_insertion_sort(t_stack **a, t_stack **b, int elements)
 	int			largest_size;
 	t_number	*curr;
 	t_number	*beg;
+	int			min;
 
 	ft_display_stacks(*a, *b);
 	ft_putchar('\n');
@@ -113,9 +114,41 @@ void	ft_insertion_sort(t_stack **a, t_stack **b, int elements)
 		if ((**b).first->n > ft_get_last(b))
 			ft_reverse_rotate(b);	
 	}
-	
+	printf("b adjustments\n");
 	ft_display_stacks(*a, *b);
 	ft_putchar('\n');
 
-	
+	//Insertion of b
+	min = (**a).first->n;
+	while ((**b).first)
+	{
+		ft_display_stacks(*a, *b);
+		ft_putchar('\n');
+		if ((**a).first->n < (**b).first->n && (**a).first->n == min)
+			ft_push(b, a);
+		else if ((**a).first->n < (**b).first->n && (**a).first->n != min)
+			ft_rotate(a);
+		else if ((**a).first->n > (**b).first->n && (**b).first->n > ft_get_last(a))
+		{
+			ft_push(b, a);
+			min = ((**a).first->n < min) ? (**a).first->n : min;
+		}
+		else if ((**a).first->n > (**b).first->n && (**b).first->n < ft_get_last(a))
+			ft_reverse_rotate(a);
+		if ((**b).first && (**b).first->next)
+		{
+			if ((**b).first->n > (**b).first->next->n)
+				ft_swap(b);
+			if ((**b).first->next->n < (**a).first->n && (**b).first->next->n > ft_get_last(a))
+			{
+				ft_swap(b);
+				ft_push(b, a);
+				min = ((**a).first->n < min) ? (**a).first->n : min;
+			}
+		}
+	}
+	while ((**a).first->n > ft_get_last(a))
+		ft_rotate(a);
+	ft_display_stacks(*a, *b);
+	ft_putstr("end\n");
 }
